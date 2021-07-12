2a83bfc28124d82351d59559b07059a1d45f54b5
Emmanuel <eayala@est.frba.utn.edu.ar>
2021-07-12 20:39:16 -0300

    Agrego Armadores de mensajes

--- TP_Rescate_de_patitas/src/main/java/domain/models/entities/utils/ArmadoresDeMensajes/ArmadorMensajeAdoptanteADuenio.java
+++ TP_Rescate_de_patitas/src/main/java/domain/models/entities/utils/ArmadoresDeMensajes/ArmadorMensajeAdoptanteADuenio.java
@@ -1,2 +1,27 @@
-package domain.models.entities.utils.ArmadoresDeMensajes;public class ArmadorMensajeAdoptanteADuenio {
+package domain.models.entities.utils.ArmadoresDeMensajes;
+
+import domain.models.entities.entidadesGenerales.Contacto;
+import domain.models.entities.entidadesGenerales.personas.Persona;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArmadorMensajeAdoptanteADuenio implements ArmadorDeMensaje {
+    Persona personaRemitente;
+
+    public ArmadorMensajeAdoptanteADuenio(Persona personaRemitente){
+        this.personaRemitente = personaRemitente;
+
+    }
+
+    public String obtenerTelefonosContacto(Persona personaRemitente){
+        List<Contacto> contactosPersona = personaRemitente.getContactos();
+        List<String> telefonos = contactosPersona.stream().map(contacto -> contacto.getTelefono()).collect(Collectors.toList());
+        String telefonosStr = telefonos.toString();
+        return telefonosStr;
+    }
+    public String armarCuerpoMensaje(){
+        String cuerpoMensaje ="Hola! mi nombre es " + personaRemitente.getNombre() +" " + personaRemitente.getApellido() + " me interesaría adoptar a tu mascota, podes contactarte conmigo a cualquiera de estos numeros:" + obtenerTelefonosContacto(personaRemitente);
+        return cuerpoMensaje;
+    }
 }
--- TP_Rescate_de_patitas/src/main/java/domain/models/entities/utils/ArmadoresDeMensajes/ArmadorMensajeRescatistaADuenio.java
+++ TP_Rescate_de_patitas/src/main/java/domain/models/entities/utils/ArmadoresDeMensajes/ArmadorMensajeRescatistaADuenio.java
@@ -1,2 +1,27 @@
-package domain.models.entities.utils.ArmadoresDeMensajes;public class ArmadorMensajeRescatistaADuenio {
+package domain.models.entities.utils.ArmadoresDeMensajes;
+
+import domain.models.entities.entidadesGenerales.Contacto;
+import domain.models.entities.entidadesGenerales.personas.Persona;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArmadorMensajeRescatistaADuenio implements ArmadorDeMensaje {
+    Persona personaRemitente;
+
+    public ArmadorMensajeRescatistaADuenio(Persona personaRemitente){
+        this.personaRemitente = personaRemitente;
+
+    }
+
+    public String obtenerTelefonosContacto(Persona personaRemitente){
+        List<Contacto> contactosPersona = personaRemitente.getContactos();
+        List<String> telefonos = contactosPersona.stream().map(contacto -> contacto.getTelefono()).collect(Collectors.toList());
+        String telefonosStr = telefonos.toString();
+        return telefonosStr;
+    }
+    public String armarCuerpoMensaje(){
+        String cuerpoMensaje ="Hola! mi nombre es " + personaRemitente.getNombre() +" " + personaRemitente.getApellido() + " encontré a tu mascota perdida, podes contactarte conmigo a cualquiera de estos numeros:" + obtenerTelefonosContacto(personaRemitente);
+        return cuerpoMensaje;
+    }
 }
