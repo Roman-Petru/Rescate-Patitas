3627b86d89e253645643d89b68526831aee7864b
Roman-Petru <ropetru@hotmail.com>
2021-10-09 15:39:26 -0300

    Agregado Spark y Handlebars

---
+++ TP_Rescate_de_patitas/src/main/java/domain/server/Router.java
@@ -0,0 +1,28 @@
+package domain.server;
+
+import domain.spark.utils.BooleanHelper;
+import domain.spark.utils.HandlebarsTemplateEngineBuilder;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+public class Router {
+    private static HandlebarsTemplateEngine engine;
+
+    private static void initEngine() {
+        Router.engine = HandlebarsTemplateEngineBuilder
+                .create()
+                .withDefaultHelpers()
+                .withHelper("isTrue", BooleanHelper.isTrue)
+                .build();
+    }
+
+    public static void init() {
+        Router.initEngine();
+        Spark.staticFileLocation("/public");
+        Router.configure();
+    }
+
+    private static void configure(){
+        Spark.get("/", (req, res) -> "hello world");
+    }
+}
