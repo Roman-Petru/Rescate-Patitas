d58c643065772a9157515d2a3bdacf1ba452b6c4
Roman-Petru <ropetru@hotmail.com>
2021-10-13 18:53:37 -0300

    minor test fixes

--- TP_Rescate_de_patitas/src/test/java/domain/testPersistencia/RepositoriosConDB.java
+++ TP_Rescate_de_patitas/src/test/java/domain/testPersistencia/RepositoriosConDB.java
@@ -26,7 +26,9 @@ public class RepositoriosConDB {
         ubicacion.setLongitud(58.66555);
 
 
-        DatosDePersona datosDePersona = new DatosDePersona("Chicle", "Pavo", "3535", "53535", "asddfsdf@hotmail.com", ubicacion, null);
+        DatosDePersona datosDePersona = new DatosDePersona("Chicle", "Pavo", "489", "53535", "asddfsdf@hotmail.com", ubicacion, null);
+
+
         DatosDePersona.DatosDePersonaDTO dto = datosDePersona.toDTO();
 
         PersonaController.getInstancia().agregar(dto);
@@ -37,16 +39,20 @@ public class RepositoriosConDB {
     @Test
     public void testUpdatePersona() {
 
-        Ubicacion ubicacion = new Ubicacion();
+/*        Ubicacion ubicacion = new Ubicacion();
         ubicacion.setDireccion("Los hornos 4599, Buenos Aires");
         ubicacion.setLatitud(-35.814884);
-        ubicacion.setLongitud(58.66555);
+        ubicacion.setLongitud(58.66555);*/
 
-        DatosDePersona datosDePersona = new DatosDePersona("Ramita", "Gonzalez", "3535", "53535", "asddfsdf@hotmail.com", ubicacion, null);
+        //DatosDePersona datosDePersona = new DatosDePersona("Ramita", "Gonzalez", "3535", "53535", "asddfsdf@hotmail.com", null, null);
+        DatosDePersona datosDePersona = PersonaController.getInstancia().buscarPersonaporDNI("489");
+        datosDePersona.setNombre("Ramita");
         DatosDePersona.DatosDePersonaDTO dto = datosDePersona.toDTO();
 
+
+
         //TODO crea ubicacion nuevamente
-        PersonaController.getInstancia().modificar(1, dto);
+        PersonaController.getInstancia().modificar(dto.getId(), dto);
 
         //assert
     }
