12309611a507f712007a0dd7683c22335e47e404
Roman-Petru <ropetru@hotmail.com>
2021-10-13 04:51:47 -0300

    generador QR

---
+++ TP_Rescate_de_patitas/src/main/java/domain/models/modulos/generadorQR/generadorQR.java
@@ -0,0 +1,31 @@
+package domain.models.modulos.generadorQR;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class generadorQR {
+
+    public static void generar(Integer mascotaID) {
+        BitMatrix matrix;
+
+        {
+            try {
+                String data = "http://localhost:9000/rescateConChapita/" + mascotaID;
+                String path = "./codigoQR" + mascotaID + ".jpg";
+
+                matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, 500, 500);
+                MatrixToImageWriter.writeToPath(matrix, "jpg", Paths.get(path));
+            } catch (WriterException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
